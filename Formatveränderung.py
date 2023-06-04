407f2f3ff812fec3a0707629cada28571dedfc0b
Jiaming Yu <131684603+Jiamingyu1@users.noreply.github.com>
2023-06-04 20:46:43 +0200

    Update Formatveränderung.py

--- Formatveränderung.py
+++ Formatveränderung.py
@@ -1,13 +1,6 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 import os
-
 class2id = {'dirt': 0, 'business_cards': 1, 'keys': 2, 'other': 3, 'paper_and_notebooks': 4, 'pens': 5, 'rulers': 6,
-            'scissors': 7,
-            'tapes': 8, 'usb_sticks': 9}
-
+            'scissors': 7, 'tapes': 8, 'usb_sticks': 9}
 
 def process_file(filename):
     # Open the input file
@@ -29,23 +22,18 @@ def process_file(filename):
     with open(filename, 'w') as file:
         file.writelines(modified_lines)
 
-
 def batch_process_files(directory):
     # Get a list of all files in the directory
     files = sorted(os.listdir(directory))
-
     # Process each file in the directory
     for filename in files:
         # Check if the file is a text file
         if filename.endswith(".txt"):
             # Get the full path of the file
             file_path = os.path.join(directory, filename)
-
             # Process the file
             process_file(file_path)
-
     print("Batch processing complete!")
-
 
 # Usage example
 batch_process_files('/Users/jiamingyu/Desktop/ultralytics-main/datasets/dirtdtataset/train/labels1')
