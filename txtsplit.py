5de1e3ce9a385ec846cbcf3a575bee73b9f7d344
Jiaming Yu <131684603+Jiamingyu1@users.noreply.github.com>
2023-06-04 20:47:29 +0200

    Update txtsplit.py

--- txtsplit.py
+++ txtsplit.py
@@ -1,14 +1,8 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
 def split_file(filename):
     with open(filename, 'r') as file:
         for line in file:
             # Split the line into columns
             columns = line.split()
-
             if len(columns) > 0:
                 # Extract the first column
                 first_column = columns[0]
@@ -26,6 +20,3 @@ def split_file(filename):
 
 # Usage example
 split_file('bbox_labels.txt')
-
-
-
